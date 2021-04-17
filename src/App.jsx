a4e34f7288d0f8c1c68422899fd1d71a09dae6e3
UniversessH <universess@qq.com>
2021-04-17 14:29:53 +0800

    完成主页，部分用户页面

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Mainpage from './pages/Mainpage'
-//import Mypage from './pages/MyPage'
+import MyPage from './pages/MyPage'
+import Setting from './pages/Setting'
+import BindEmail from './pages/BindEmail'
+import PersonInfo from './pages/PersonInfo'
 import './App.css';
 //import menu from './img/menu.png';
 //import map from './img/map.jpg'
@@ -14,10 +17,14 @@ export default class App extends Component {
     return (
       <div>
         <Switch>
-          <Route path="/mainpage" component={Mainpage}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/register" component={Register}></Route>
-          <Redirect to="/mainpage" />
+          <Route path="/tudo" exact={true} component={Mainpage}></Route>
+          <Route path="/tudo/mypage" component={MyPage}></Route>
+          <Route path="/tudo/login" component={Login}></Route>
+          <Route path="/tudo/register" component={Register}></Route>
+          <Route path="/tudo/setting" component={Setting}></Route>
+          <Route path="/tudo/bindemail" component={BindEmail}></Route>
+          <Route path="/tudo/personinfo" component={PersonInfo}></Route>
+          <Redirect to="/tudo" />
         </Switch>
       </div>
     )
---
+++ src/pages/PersonInfo/index.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import styles from './index.module.css'
+import backButton from '../../img/back.png'
+import Portrait from '../../img/portrait.png'
+
+export default class PersonInfo extends Component {
+  //鉴权
+  componentDidMount() {
+    if (!localStorage.getItem('token')) {
+      this.props.history.push('/tudo/login')
+    }
+  }
+  To_Setting = () => {
+    this.props.history.push('/tudo/setting')
+  }
+  render() {
+    return (
+      <div className={styles.body_div}>
+        <div className={styles.backButton_div}>
+          <img className={styles.backButton} src={backButton} alt="backImg" onClick={this.To_Setting} />
+          <span className={styles.InfoTitle}>个人信息</span> <div></div>
+        </div>
+        <div className={styles.between1}></div>
+        <div className={styles.Side_up_div}>
+          <span>头像</span>
+          <img src={Portrait} alt="Portrait" className={styles.Portrait}></img>
+        </div>
+        <div className={styles.Middle_div} onClick={this.BindEmail}>
+          <span>昵称</span>
+        </div>
+        <div className={styles.Middle_div}>
+          <span>学号</span>
+        </div>
+        <div className={styles.Middle_div}>
+          <span>性别</span>
+        </div>
+        <div className={styles.Side_down_div}>
+          <span>个性签名</span>
+        </div>
+
+        <div className={styles.bottom}></div>
+
+      </div>
+    )
+  }
+}
